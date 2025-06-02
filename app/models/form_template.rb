0d6ded5214cc3d097a70480aaaee56c5703da9bc
Rafael Guzman <rega1237@gmail.com>
2025-06-02 17:07:19 -0400

    feat(form_template): add callback to update form fills when structure changes
    Add before_update callback to synchronize form_structure changes to all associated form_fill records. This ensures data consistency when the template structure is modified.

--- app/models/form_template.rb
+++ app/models/form_template.rb
@@ -7,6 +7,7 @@ class FormTemplate < ApplicationRecord
   # validates :file_type, presence: true
   # validates :form_structure, presence: true
 
+  before_update :update_associated_form_fills, if: :form_structure_changed?
   before_destroy :delete_file_from_google_drive
 
   private
@@ -15,5 +16,11 @@ class FormTemplate < ApplicationRecord
     return unless google_drive_file_id.present?
 
     GoogleDriveService.new.delete_file(google_drive_file_id)
+  end
+
+  def update_associated_form_fills
+    form_fills.each do |form_fill|
+      form_fill.update(form_structure: form_structure)
+    end
   end
 end
