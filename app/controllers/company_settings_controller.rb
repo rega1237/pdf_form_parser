f2480f25f2db9b4da74a7b91e7adf9d21df04916
Rafael Guzman <rega1237@gmail.com>
2025-07-08 14:27:29 -0400

    feat(company_settings): add users and new user to controller
    Fetch and order users by creation date and initialize new user instance for the form

--- app/controllers/company_settings_controller.rb
+++ app/controllers/company_settings_controller.rb
@@ -3,5 +3,8 @@ class CompanySettingsController < ApplicationController
     @deficiencies = Deficiency.all
     @intervals = IntervalCategory.all
     @system_categories = SystemCategory.all
+
+    @users = User.all.order(created_at: :desc)
+    @new_user = User.new
   end
 end
