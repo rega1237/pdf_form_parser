21676cd3d6ca58fd0615c123e9eff545785999b4
Rafael Guzman <rega1237@gmail.com>
2025-07-08 14:26:42 -0400

    feat(users): add admin users management and update registration
    - Add new admin users controller with CRUD operations
    - Remove :registerable from User model to disable self-registration
    - Add user_level and status to permitted sign up parameters

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,53 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[edit update destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+    # Asigna la contraseña por defecto
+    @user.password = 'aespro1234'
+    @user.password_confirmation = 'aespro1234'
+
+    if @user.save
+      redirect_to settings_path, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    # Filtra los parámetros para no requerir la contraseña si está en blanco
+    params_to_update = user_params
+    if params_to_update[:password].blank?
+      params_to_update.delete(:password)
+      params_to_update.delete(:password_confirmation)
+    end
+
+    if @user.update(params_to_update)
+      redirect_to admin_users_path, notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to settings_path, notice: 'User was successfully destroyed.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
