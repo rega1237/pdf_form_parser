6e7693fcfe3354d3cae7c30b71da0e2538830440
Rafael Guzman <rega1237@gmail.com>
2025-09-08 12:43:01 -0400

    feat(datepicker): add flatpickr integration for corrective action dates
    - Add flatpickr dependency and importmap configuration
    - Create datepicker stimulus controller
    - Update form field partial to use datepicker for corrective action sections
    - Include flatpickr CSS in application layout

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'sortablejs' # @1.15.6
 pin '@rails/ujs', to: 'rails-ujs.js'
+pin "flatpickr" # @4.6.13
