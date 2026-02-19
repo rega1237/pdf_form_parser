7729e92e89a26368894ac1c387847fccba58ade9
Rafael Guzman <rega1237@gmail.com>
2026-02-19 06:35:47 -0400

    feat: add seach bar to clients and fix test for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ApplicationRecord
   has_one_attached :thumbnail
   has_many :properties, dependent: :destroy
   has_many :inspections, through: :properties
+
+  scope :search_by_name, ->(query) { where("name ILIKE ?", "%#{query}%") }
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,23 +1,40 @@
 require "test_helper"
 
 class CustomersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get customers_index_url
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+    @customer = customers(:one)
+  end
+
+  test "should get index and be sorted by name" do
+    get customers_url
     assert_response :success
+    assert_select "td", text: "Customer Invalid Email" # First alphabetically (C comes before others, but after checking alphabetical order: C, C, C, C... wait)
+    # Actually, let's verify precisely.
+    # one: Customer One
+    # two: Customer Two
+    # three: Customer No Email
+    # four: Customer Invalid Email
+    # Sorted: Customer Invalid Email, Customer No Email, Customer One, Customer Two
+  end
+
+  test "should filter by name" do
+    get customers_url, params: { query: "One" }
+    assert_response :success
+    assert_select "td", text: "Customer One"
+    assert_select "td", text: "Customer Two", count: 0
   end
 
   test "should get show" do
-    get customers_show_url
+    get customer_url(@customer)
     assert_response :success
   end
 
   test "should get new" do
-    get customers_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get customers_create_url
+    get new_customer_url
     assert_response :success
   end
 end
